b301343a476c9641feddd0f056a983de8494caa7
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-07-23 20:40:16 +0900

    Update Aireviewresult2.jsx

--- src/components/Aireviewresult2.jsx
+++ src/components/Aireviewresult2.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 import suggestcontract from "../images/suggestcontract.svg";
 import { modifiedContract } from "../services/getModifiedContract";
